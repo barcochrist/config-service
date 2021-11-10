aa57252f065f5d56e64d4a2011692923c95d471f
Christian Barco <barcochrist@gmail.com>
2021-11-10 17:09:11 -0500

    (init): add basic project structure

---
+++ src/main/java/co/programacionmaster/tumesero/config/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package co.programacionmaster.tumesero.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
